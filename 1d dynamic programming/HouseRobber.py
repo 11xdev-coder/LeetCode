ce3d6e2637c12f94a92c3804135ad1f2e2dbc762
Matvei Piashchynski <piashchynski.matvei@gmail.com>
2026-01-17 19:32:41 +0300

    house robber

---
+++ 1d dynamic programming/HouseRobber.py
@@ -0,0 +1,17 @@
+
+def rob(nums: List[int]) -> int:
+    N = len(nums)
+    if N <= 1:
+        return nums[0]
+    dp = [0] * N
+ 
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+
+    for i in range(2, N):
+        dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+    return dp[N-1]
+
+nums = list(map(int, input().split()))
+print(rob(nums))
