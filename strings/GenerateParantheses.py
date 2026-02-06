9e58a9aeb0180a309c0035c5e620eab13f9724ed
qsef <piashchynski.matvei@gmail.com>
2026-02-06 19:55:40 +0300

    Generate Parantheses

---
+++ strings/GenerateParantheses.py
@@ -0,0 +1,34 @@
+def solve(n: int) -> List[str]:
+    def open(arr, pair, op, cl):
+        pair_copy = pair[::]
+        pair_copy.append("(")
+        arr.append((pair_copy, op+1, cl))
+    def close(arr, pair, op, cl):
+        pair_copy = pair[::]
+        pair_copy.append(")")
+        arr.append((pair_copy, op, cl+1))
+        
+    cur_pairs = [(["("], 1, 0)]
+
+    while len(cur_pairs[0][0]) < 2*n:
+        added = []
+        for cur_pair in cur_pairs:
+
+            pair, opened, closed = cur_pair
+            if opened > closed:
+                if opened < n:
+                    open(added, pair, opened, closed)
+                close(added, pair, opened, closed)
+            else:
+                open(added, pair, opened, closed)
+
+            cur_pairs = added[::]
+
+    ans = []
+    for (pair, op, cl) in cur_pairs:
+        ans.append("".join(pair))
+
+    return ans
+
+N = int(input())
+print(solve(N))
